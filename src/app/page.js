e74f9e66e094c2582865fd82badadc66cd163bd5
ashein-codethedreams <heroglasses155@gmail.com>
2024-08-05 14:38:28 +0630

    init tweetId input

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
+"use client";
 import TweetComponent from "@/components/Tweet";
+import { Button, Input } from "antd";
+import { useState } from "react";
 
 export default function Home() {
-  const tweetId = "1819797937414611313";
+  // const tweetId = "1819756198666576067";
+  const [tweetId, setTweetId] = useState(null);
+  const onTweetId = () => {
+    setTweetId("1819756198666576067");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Tweet Example in Next.js 13.4</h1>
+    <main className="flex min-h-screen bg-white flex-col items-center justify-between py-10">
+      <div className="flex">
+        <Input type="text" placeholder="enter tweet id" />
+        <Button onClick={() => onTweetId()} type="primary">
+          Test
+        </Button>
+      </div>
+
       <TweetComponent tweetId={tweetId} />
     </main>
   );
